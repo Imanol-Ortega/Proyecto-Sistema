0f9ad7489078fc52682acba2b26487ffd1616909
Imanol-Ortega <59693036+Imanol-Ortega@users.noreply.github.com>
2023-09-28 20:49:22 -0400

    a

--- client/src/api/prueba.api.js
+++ client/src/api/prueba.api.js
@@ -2,6 +2,7 @@ import axios from 'axios'
 
 export const getPruebaRequest = async()=>{
     const response = await axios.get('http://localhost:4000/prueba')
+    console.log(response.data.rows) 
     return response
     
 };
